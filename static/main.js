d72676a71d593f53715e2a662b066528a98827a8
John Exterkamp <61565251+ProgrammingJohn@users.noreply.github.com>
2024-09-05 17:46:16 -0400

    update

--- static/main.js
+++ static/main.js
@@ -1,12 +1,15 @@
 $("#linkedIn").click(function () {
   window.open("https://www.linkedin.com/in/johnexterkamp/", "_blank");
 });
+function reset(tool, val) {
+  setTimeout(() => {
+    tool.find(".tooltiptext").text(val);
+  }, 1000);
+}
 $(".tooltip").click(function () {
   tool = $(this);
   val = tool.find(".tooltiptext").text();
   navigator.clipboard.writeText(val);
   tool.find(".tooltiptext").text("Copied !");
-  setTimeout(() => {
-    tool.find(".tooltiptext").text(val);
-  }, 1000);
+  reset(tool, val);
 });
